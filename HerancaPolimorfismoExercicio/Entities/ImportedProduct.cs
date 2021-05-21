0f5159f1ef6e7f1003cfe6e9c819391adb87a5cc
Anderson Pereira <anderson.pereira@viannasempre.com.br>
2021-05-20 21:17:34 -0300

    Exemplos calros de Herança e Polimorfismo - Exercício

---
+++ HerancaPolimorfismoExercicio/Entities/ImportedProduct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerancaPolimorfismoExercicio.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct() { }
+
+        public ImportedProduct(string name, double price, double customsFee) :
+            base(name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public override string PriceTag()
+        {
+            double valor = Price + CustomsFee;
+            return Name + " $ " + valor.ToString("F2", CultureInfo.InvariantCulture) + " (Taxa Alfandegaria: $ " + CustomsFee +")";
+        }
+    }
+}
---
+++ HerancaPolimorfismoExercicio/Program.cs
@@ -0,0 +1,57 @@
+﻿using HerancaPolimorfismoExercicio.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace HerancaPolimorfismoExercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> lista = new List<Product>();
+
+            Console.Write("Informe a quantidade de Produtos: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Informação Produto #{i}: ");
+                Console.Write("Normal, usadado ou importado (c/u/i)? ");
+                char ch = char.Parse(Console.ReadLine().ToLower());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Price: ");
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (ch.Equals('i'))
+                {
+                    Console.Write("Taxa Alfandegária: ");
+                    double taxa = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Product produtoImportado = new ImportedProduct(name, preco, taxa);
+                    lista.Add(produtoImportado);
+                }
+                else if (ch.Equals('u'))
+                {
+                    Console.Write("Data de Fabricação (DD/MM/YYYY): ");
+                    DateTime data = DateTime.Parse(Console.ReadLine());
+                    Product produtoUsado = new UsedProduct(name, preco, data);
+                    lista.Add(produtoUsado);
+                }
+                else
+                {
+                    Product produto = new Product(name, preco);
+                    lista.Add(produto);
+                }
+            }
+            Console.WriteLine("\nPRICE TAGS: ");
+            foreach (Product item in lista)
+            {
+                    Console.WriteLine(item.PriceTag());
+            }
+            Console.ReadKey();
+        }
+    }
+}
